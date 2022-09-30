7cb36d609ff15b6d14e70826b7e0057eb5ccfd12
sudoshu <sudoshu@protonmail.com>
2022-09-30 14:39:23 -0400

    fix(ci): update semantic versioning workflow

--- cmd/qst/main.go
+++ cmd/qst/main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"github.com/quantstop/quantstopterminal/internal/engine"
 	"github.com/quantstop/quantstopterminal/internal/log"
+	"os"
+	"strings"
+)
+
+var (
+	// these will be replaced by goreleaser
+	version = "v0.0.0"
+	commit  = "0000000"
+	date    = "0001-01-01T00:00:00Z"
 )
 
 func main() {
+
+	// parse goreleaser info
+	if len(os.Args) >= 2 && "version" == strings.TrimPrefix(os.Args[1], "") {
+		fmt.Printf("YOUR_CLI_NAME v%s %s (%s)\n", version, commit[:7], date)
+	}
 
 	log.Debugln(log.Global, "Creating Engine ...")
 	err, bot := engine.NewEngine()
